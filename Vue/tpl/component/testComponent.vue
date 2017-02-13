ccfdb26cdb8310431225ef11e1db707149d4e4c5
GZhiDao1010 <GZhiDao1010@gmail.com>
2017-02-13 10:28:25 +0800

    es6

---
+++ Vue/tpl/component/testComponent.vue
@@ -0,0 +1,15 @@
+<template>
+	<h1>i am testComponent</h1>
+	<h3>written by {{author}}</h3>
+</template>
+
+<script>
+export default {
+	data(){
+		return {
+			author:"GZhiDao"
+		}
+	}
+}
+
+</script>
