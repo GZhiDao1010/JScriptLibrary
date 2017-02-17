c1842b7dfc6acc88416859a7d7478359a0b0e627
GZhiDao1010 <GZhiDao1010@gmail.com>
2017-02-17 17:48:53 +0800

    game

--- Project/大海捞红包/src/js/redSeaFish.js
+++ Project/大海捞红包/src/js/redSeaFish.js
@@ -1,5 +1,6 @@
-$(function(){
+var redSeaFish = {};
 
+redSeaFish.home = function(){ //首页
 	var musicComponent = { //音乐播放
 			speed:10,//旋转速度
 			direction:1,//1 顺时针 -1为逆时针
@@ -172,4 +173,143 @@ $(function(){
 	$('#popupCloseBtn').on('click',function(){
 		closePopupBox();
 	})
-})
+};
+redSeaFish.secondsCountdown = function(){ //30秒倒计时
+	var msg = {
+			time:5, //倒计时长 单位：s
+			id:'sec' //显示的dom
+	}
+	var num = setInterval(function(){
+		document.getElementById(msg.id).innerHTML = --msg.time;
+		if(msg.time <= 0){ //时间停止
+			redSeaFish.timeOut();
+			clearInterval(num);
+		}
+	},1000)
+}
+redSeaFish.timeOut = function(){ //30秒倒计时结束执行方法
+	alert('方法名：redSeaFish.timeOut()')
+}
+redSeaFish.canvasHeight = function(){ //画布自适应高度
+	var wHeight = $(window).height()*0.35;
+	return $('#gameWorkPlace').css({
+		"height":wHeight+'px'
+	})
+}
+redSeaFish.staticGame = function(){ //静态动画
+	this.canvasHeight();
+	/*var imgGroup = {
+			img:function(){
+				var imgz = new Image();  
+				imgz.src = "../img/stars.png"; 
+				imgz.style.width = "100px";
+				imgz.style.height = "100px";
+				return imgz;
+			}
+	}*/
+	var moveImgs = $('.move_imgs')[0];
+	var shipsWidth = $('.move_imgs')[0].width*0.2;
+	var shipsHeight = $('.move_imgs')[0].height*0.2;
+	console.log(shipsWidth+'--'+shipsHeight)
+	console.log($('.move_imgs')[0].height)
+	var mycv = document.getElementById("gameWorkPlace");  
+	var myctx = mycv.getContext("2d");
+	myctx.drawImage(moveImgs, 0, 0,shipsWidth,shipsHeight);
+	
+	
+}
+redSeaFish.playGame = function(){ //玩游戏...
+	
+	
+}
+redSeaFish.playRule = function(){ //游戏规则、奖品页
+	var topHeight = $('.rule_top_bg')[0].offsetHeight; 
+	var windowHeight = $(window).height();
+	$('.rule_top').css({"height":topHeight + 'px'})
+	$('#rule_middle_cont').css({"height":windowHeight - topHeight + 10 +'px',"top":topHeight - 20 + 'px'});
+}
+redSeaFish.removPopue = function(){ //删除弹框
+	return $(document).find('.popup_box').remove();
+}
+redSeaFish.notWinning = function(){ //未中奖情况
+	var ishas = $(document).find('.popup_box').length;
+	if(ishas != 0){
+		$(document).find('.popup_box').remove();
+	}
+	
+	var msg = [ //未中奖信息
+	            {src:'../img/notwin1.png',text:'加把劲哟！我准备好红包，你储备好力气！'},
+	            {src:'../img/notwin2.png',text:'捞的不够努力，红包与你擦肩 而过、再试一次！'},
+	            {src:'../img/notwin3.png',text:'换个姿势放大招！'},
+	 	    ];
+	var num = Math.floor(Math.random()*msg.length);
+	var h = '<section class="popup_box" title="弹框背景" style="display:block">'+
+	'	<section class="popup_cont_box" title="弹框内容" style="text-align:center;">'+
+	'		<section class="popup_title_box"style="background-color:#eb0056;text-align:center;height: 15px;border-radius:5px 5px 0 0;">'+
+	'			<a href="javascript:;" onclick="redSeaFish.removPopue()" class="popup_close_btn" id="popupCloseBtn" style="top: -4px; font-size: 29px;">×</a>'+
+	'		</section>'+
+	'		<section class="popup_cont"  style="background-color:#eb0056;text-align:center;color:#f6ff2e;border-radius:0 0 5px 5px ;font-size:18px;">'+
+	'			<section class="popup_cont_text">'+msg[num].text+'</section>'+
+	'			<img class="popup_erweima" style="width:50%;padding:5px 25%" src="'+msg[num].src+'">'+
+	'			<button class="play_agin" onclick="redSeaFish.playAgainGame()">再玩一次</button>'+
+	'		</section>'+
+	'	</section>'+
+	'</section>';
+	$('body').append(h);
+}
+redSeaFish.playAgainGame = function(){ //再玩一次
+	alert("方法名：redSeaFish.playAgainGame()")
+}
+redSeaFish.getWinning = function(){ //中奖显示
+	var ishas = $(document).find('.popup_box').length;
+	if(ishas != 0){
+		$(document).find('.popup_box').remove();
+	}
+	var msg = [ //未中奖信息
+	            {src:'../img/win1.png',text:'恭喜你捞到一个红包'},
+	 	    ];
+	var h = '<section class="popup_box" title="弹框背景" style="display:block">'+
+	'	<section class="popup_cont_box" title="弹框内容" style="text-align:center;">'+
+	'		<section class="popup_title_box"style="background-color:#eb0056;text-align:center;height: 25px;border-radius:5px 5px 0 0;">'+
+	'			<a href="javascript:;" onclick="redSeaFish.removPopue()" class="popup_close_btn" id="popupCloseBtn" style="top: -4px; font-size: 29px;">×</a>'+
+	'		</section>'+
+	'		<section class="popup_cont"  style="background-color:#eb0056;position: relative;text-align:center;color:#f6ff2e;border-radius:0 0 5px 5px ;font-size:18px;">'+
+	'			<section class="popup_cont_text" style="position: absolute;text-align: center;left: 0;width: 100%;font-size: 20px;top: 38%;">'+msg[0].text+'</section>'+
+	'			<img class="popup_erweima" style="width:100%;padding:5px 0" src="'+msg[0].src+'">'+
+	'			<button class="play_agin" onclick="redSeaFish.getToGift()">立即领取</button>'+
+	'		</section>'+
+	'	</section>'+
+	'</section>';
+	$('body').append(h);
+}
+redSeaFish.getToGift = function(){ //立即领取
+	alert("方法名：redSeaFish.getToGift()")
+}
+redSeaFish.fillInWinMsg = function(){ //填写中奖信息
+	var ishas = $(document).find('.popup_box').length;
+	if(ishas != 0){
+		$(document).find('.popup_box').remove();
+	}
+	var msg = [ //未中奖信息
+	            {src:'../img/win1.png',text:'恭喜你捞到一个红包'},
+	 	    ];
+	var h = '<section class="popup_box" title="弹框背景" style="display:block" >'+
+	'	<section class="popup_cont_box" title="弹框内容" style="text-align:center;">'+
+	'		<section class="fill_in_msg">'+
+	'			<section class="fill_in_msg_line"></section>'+
+	'		</section>'+
+	'		<section class="fill_in_inputs">'+
+	'			<section class="row">手机号：<input type="text" class="input_number" /></section>'+
+	'			<section class="row">验证码：<input type="text" class="input_number" style="width:30%;"  /><button class="get_yzm">获取验证码</button></section>'+
+	'			<section>'+
+	'				<button class="play_agin" onclick="redSeaFish.removPopue()">取消</button>'+
+	'				<button class="play_agin" onclick="redSeaFish.submitManMsg()">提交</button>'+
+	'			</section>'+
+	'		</section>'+
+	'	</section>'+
+	'</section>';
+	$('body').append(h);
+}
+redSeaFish.submitManMsg = function(){ //提交领奖信息
+	alert("方法名：redSeaFish.submitManMsg()")
+}
